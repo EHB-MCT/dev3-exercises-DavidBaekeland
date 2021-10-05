e227207f1759af6b60d10075c05bf26408f32c89
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-10-05 16:50:55 +0200

    2de versie: hello world + random nummer

--- session1/src/main/kotlin/Main.kt
+++ session1/src/main/kotlin/Main.kt
@@ -3,12 +3,25 @@ var poeple = arrayOf("Hira", "Jens", "Jasmien", "Tyron", "Stijn")
 
 fun main () {
     println("Hello World")
-    printNames()
-}
-
-fun printNames () {
     // https://stackoverflow.com/questions/10227408/foreach-in-kotlin
     for(name in poeple) {
-        println("Hello " + name)
+        printNames(name)
     }
+
+    randomNumber()
+}
+
+fun printNames (name: String) {
+    println("Hello $name")
+}
+
+fun randomNumber () {
+    //https://stackoverflow.com/questions/45685026/how-can-i-get-a-random-number-in-kotlin
+    // val randomNumber = (0..2).random() -> Je kan random gebruiken op alle array
+    var options = arrayOf("rock", "paper", "scissors").random()
+    println(options)
+}
+
+fun askNumber () {
+    
 }
