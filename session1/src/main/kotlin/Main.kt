8c404e796053b0e2a16b3dde63e20b43c2ab3e9b
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-10-05 16:24:25 +0200

    Dynamic Hello world

--- session1/src/main/kotlin/Main.kt
+++ session1/src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
+// https://www.geeksforgeeks.org/kotlin-array/
+var poeple = arrayOf("Hira", "Jens", "Jasmien", "Tyron", "Stijn")
+
 fun main () {
     println("Hello World")
+    printNames()
+}
+
+fun printNames () {
+    // https://stackoverflow.com/questions/10227408/foreach-in-kotlin
+    for(name in poeple) {
+        println("Hello " + name)
+    }
 }
