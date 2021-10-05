5dfae8909e376f2e214285d90d0fd2a562acadfb
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-10-05 17:08:35 +0200

    rock, paper, scissors

--- session1/src/main/kotlin/Main.kt
+++ session1/src/main/kotlin/Main.kt
@@ -4,9 +4,9 @@ var poeple = arrayOf("Hira", "Jens", "Jasmien", "Tyron", "Stijn")
 fun main () {
     println("Hello World")
     // https://stackoverflow.com/questions/10227408/foreach-in-kotlin
-    for(name in poeple) {
-        printNames(name)
-    }
+    // for(name in poeple) {
+    //printNames(name)
+    //}
 
     randomNumber()
 }
@@ -18,10 +18,19 @@ fun printNames (name: String) {
 fun randomNumber () {
     //https://stackoverflow.com/questions/45685026/how-can-i-get-a-random-number-in-kotlin
     // val randomNumber = (0..2).random() -> Je kan random gebruiken op alle array
-    var options = arrayOf("rock", "paper", "scissors").random()
+    val options = arrayOf("rock", "paper", "scissors").random()
     println(options)
+    askOption(options)
 }
 
-fun askNumber () {
-    
+fun askOption (options: String) {
+    // https://beginnersbook.com/2018/09/kotlin-input-from-user/
+    print("Wich option?")
+    var answer = readLine()
+    if(answer == options)  {
+        println("You won")
+    } else {
+        println("Sorry. Wrong answer")
+    }
+
 }
