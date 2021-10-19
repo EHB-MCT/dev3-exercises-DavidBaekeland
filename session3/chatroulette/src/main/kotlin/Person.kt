d92de8c36792fcf4d525eecb1f7a3c82bd5befa1
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-10-19 15:47:48 +0200

    gitignore, main: person: array, random, introduce

---
+++ session3/chatroulette/src/main/kotlin/Person.kt
@@ -0,0 +1,5 @@
+class Person(val name: String) {
+    fun introduce() {
+        println("Hello I am " + name)
+    }
+}
---
+++ session3/chatroulette/src/main/kotlin/main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val person1 = Person("Hira")
+    val person2 = Person("Monica")
+    val poeple = arrayOf(person1, person2)
+    poeple.random().introduce()
+}
