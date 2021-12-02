bd6bd5d4944fb1e82f92d451a76cf8235a2e68ff
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-12-02 21:09:14 +0100

    Session 7: main: class: taal

--- session7/Duolingo/src/main/kotlin/Duolingo.kt
+++ session7/Duolingo/src/main/kotlin/Duolingo.kt
@@ -1,16 +1,28 @@
-class Duolingo(val size: Int = 5, val language: String="fr") {
+class Duolingo(val size: Int = 5, val language: String="eng") {
     init  {
-        val word1 = Word("Hello", "hallo", "eng")
-        val word2 = Word("Bonjour", "hallo", "fr")
-        val word3 = Word("language", "taal", "eng")
-        val word4 = Word("langue", "taal", "fr")
-        val word5 = Word("glace", "ijs", "fr")
-        val word6 = Word("contact", "contact", "eng")
-        val word7 = Word("water", "water", "eng")
-        val word8 = Word("l'eau", "water", "fr")
-        val word9 = Word("navigateur", "browser", "fr")
-        val word10 = Word("form", "formulier", "eng")
-        val words = mutableListOf<Word>(word1, word2, word3, word4, word5, word6, word7, word8, word9, word10)
+        //val word1 = Word("Hello", "hallo", "eng")
+        //val word2 = Word("Bonjour", "hallo", "fr")
+        //val word3 = Word("language", "taal", "eng")
+        //val word4 = Word("langue", "taal", "fr")
+        //val word5 = Word("glace", "ijs", "fr")
+        //val word6 = Word("contact", "contact", "eng")
+        //val word7 = Word("water", "water", "eng")
+        //val word8 = Word("l'eau", "water", "fr")
+        //val word9 = Word("navigateur", "browser", "fr")
+        //val word10 = Word("form", "formulier", "eng")
+        //val words = mutableListOf<Word>(word1, word2, word3, word4, word5, word6, word7, word8, word9, word10)
+
+        val word1 = EnglishWord("Hello", "hallo")
+        val word2 = FrenchWord("Bonjour", "hallo")
+        val word3 = EnglishWord("language", "taal")
+        val word4 = FrenchWord("langue", "taal")
+        val word5 = FrenchWord("glace", "ijs")
+        val word6 = EnglishWord("contact", "contact")
+        val word7 = EnglishWord("water", "water")
+        val word8 = FrenchWord("l'eau", "water")
+        val word9 = FrenchWord("navigateur", "browser")
+        val word10 = EnglishWord("form", "formulier")
+        val words = mutableListOf<Word2>(word1, word2, word3, word4, word5, word6, word7, word8, word9, word10)
 
 
         println(size)
@@ -23,7 +35,7 @@ class Duolingo(val size: Int = 5, val language: String="fr") {
     }
 
     // https://kotlinlang.org/docs/collections-overview.html#collection
-        fun play2(words: List<Word>)  {
+    fun play2(words: List<Word2>)  {
         val randomWords = words.shuffled().take(size).toMutableSet()
         //.shuffled = > zodat we niet elke keer 5 dezelfde hebben
         // .take() => aantal items
